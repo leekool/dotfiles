a3de90790e6ffab7a5ce7f8282e0ef22ad226cc9
leekool <leejedda@gmail.com>
2023-02-12 10:19:15 +1100

    xcolor script

--- .bin/xcolor.sh
+++ .bin/xcolor.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env sh
+
+# script for running xcolor from dmenu
+# run xcolor, copy color to clipboard, notify (dunst)
+
+COLOR=$(xcolor)
+
+echo "$COLOR" | xclip -selection clipboard
+notify-send "$COLOR"
