5e8048493bdbc483925a7d887497dfb91fcc0ddf
leekool <leejedda@gmail.com>
2023-03-27 23:52:03 +1100

    fix terminal buffer switching

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -72,9 +72,11 @@
 (defun delete-word (arg)
   (interactive "p")
   (delete-region (point) (progn (forward-word arg) (point))))
+
 (defun backward-delete-word (arg)
   (interactive "p")
   (delete-word (- arg)))
+
 (global-set-key (kbd "C-<backspace>") 'backward-delete-word)
 (global-set-key (kbd "C-<delete>") 'delete-word)
 
