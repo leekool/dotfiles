4e39e166ae376f4455d4074ebe204fb7a3dc351b
leekool <leejedda@gmail.com>
2023-04-09 13:21:04 +1000

    cover.sh

--- .config/ncmpcpp/cover.sh
+++ .config/ncmpcpp/cover.sh
@@ -17,6 +17,7 @@ COVER_SIZE=441
 CROP_BORDER=5
 
 get_cover() {
+
     ext="$(mpc --format %file% current | sed 's/^.*\.//')"
 
     if [ "$ext" = "flac" ]; then
@@ -49,12 +50,18 @@ X_PADDING=0
 Y_PADDING=0
 
 add_cover() {
+    # if no cover file, add fallback cover to avoid error
+    [ ! -f "/tmp/mpd_cover.jpg" ] &&
+        ffmpeg -y -i "$fallback_image" \
+            -vf "crop=min(in_w-$CROP_BORDER\,in_h-$CROP_BORDER):out_w,scale=-2:$COVER_SIZE" >/dev/null 2>&1 \
+            /tmp/mpd_cover.jpg
+
     ImageLayer::add [identifier]="cover" [x]="$X_PADDING" [y]="$Y_PADDING" [path]="$COVER"
 }
 
-remove_cover() {
-    ImageLayer::remove [identifier]="cover"
-}
+# remove_cover() {
+#     ImageLayer::remove [identifier]="cover"
+# }
 
 you_wait() {
     while inotifywait -q -q -e close_write "$COVER"; do
