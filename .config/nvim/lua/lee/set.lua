da7ffe3fc1804afeb654239133f78ada81ea1dee
leekool <lee@imre.al>
2024-12-13 23:15:41 +1100

    fix tabbing snippet issue

--- .config/nvim/lua/lee/set.lua
+++ .config/nvim/lua/lee/set.lua
@@ -37,3 +37,15 @@ vim.opt.colorcolumn = "80"
 vim.filetype.add({ extension = { templ = "templ" } })
 
 vim.cmd("TransparentEnable")
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+    pattern = '*',
+    callback = function()
+        if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+            and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require('luasnip').session.jump_active
+        then
+            require('luasnip').unlink_current()
+        end
+    end
+})
