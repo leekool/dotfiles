e42bdf4f6e4b808f6588001fd11667fc5219f393
leekool <lee@imre.al>
2025-01-06 19:44:10 +1100

    generic_frontmatter

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -1,16 +1,16 @@
-local work_frontmatter = function(note)
-    note:add_tag "work"
-    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-    if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-        for k, v in pairs(note.metadata) do
-            out[k] = v
+local generic_frontmatter = function(note, extra_tag)
+    local daily_tag_present = false
+    for _, tag in ipairs(note.tags or {}) do
+        if tag == "daily" then
+            daily_tag_present = true
+            break
         end
     end
-    return out
-end
 
-local real_frontmatter = function(note)
-    note:add_tag "real"
+    if not daily_tag_present then
+        note:add_tag(extra_tag)
+    end
+
     local out = { id = note.id, aliases = note.aliases, tags = note.tags }
     if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
         for k, v in pairs(note.metadata) do
@@ -27,7 +27,9 @@ require("obsidian").setup({
             path = "~/notes/real",
             strict = true,
             overrides = {
-                note_frontmatter_func = real_frontmatter
+                note_frontmatter_func = function(note)
+                    return generic_frontmatter(note, "real")
+                end,
             },
         },
         {
@@ -35,21 +37,18 @@ require("obsidian").setup({
             path = "~/notes/work",
             strict = true,
             overrides = {
-                note_frontmatter_func = work_frontmatter
+                note_frontmatter_func = function(note)
+                    return generic_frontmatter(note, "work")
+                end,
             },
         },
     },
     note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        -- In this case a note with the title 'My new note' will be given an ID that looks
-        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
         local suffix = ""
         if title ~= nil then
-            -- If title is given, transform it into valid file name.
-            suffix = title:gsub(" ", "_"):gsub("[^A-Za-z0-9-]", ""):lower()
+            suffix = title:gsub(" ", "_"):gsub("[^A-Za-z0-9-]", ""):lower() -- ensure title only contains valid chars
         else
-            -- If title is nil, just add 4 random uppercase letters to the suffix.
-            for _ = 1, 4 do
+            for _ = 1, 4 do                                                 -- if title nil, 4 random numbers as title
                 suffix = suffix .. string.char(math.random(65, 90))
             end
         end
@@ -64,6 +63,11 @@ require("obsidian").setup({
         date_format = "%d-%m-%Y",
         alias_format = "%d-%m-%Y",
         default_tags = { "daily" },
-        template = nil
+        template = "daily"
     },
+    templates = {
+        folder = "../templates",
+        date_format = "%d-%m-%Y",
+        time_format = "%H:%M",
+    }
 })
